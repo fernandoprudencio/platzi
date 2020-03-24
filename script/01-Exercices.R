3d5f128e4eee10ba8d27a8b2e6fdc22a3f3afcc9
Fernando.Prudencio.Home <fjprudenciop@gmail.com>
2020-03-23 23:12:38 -0500

    script modified

--- script/01-Exercices.R
+++ script/01-Exercices.R
@@ -1,1 +1,6 @@
-rm(list = ls())
+library(rater)
+library(tidyverse)
+df <- read.csv('data/orangeec.csv', header = T, sep = ",", stringsAsFactors = F) %>% as_tibble()# %>% View()
+View('data/orangeec.csv')
+str(df)
+
